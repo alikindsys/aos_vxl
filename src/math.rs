7f4edb1233185daf440c90c182a6e47e997df9aa
roridev <prates.aliceic@gmail.com>
2022-05-01 13:35:01 -0300

    Add `ipos2` type for getting an X,Y coordinate pair

--- src/math.rs
+++ src/math.rs
@@ -6,6 +6,18 @@ pub struct ipos8 {
     pub z: u8
 }
 
+#[allow(non_camel_case_types)]
+pub struct ipos2 {
+    pub x: u8,
+    pub y: u8
+}
+
+impl PartialEq for ipos2 {
+    fn eq(&self, other: &Self) -> bool {
+        return self.x == other.x && self.y == other.y;
+    }
+}
+
 impl ipos8 {
     /// Checks if another position has the same x,y coordinates, ignoring z.
     ///
