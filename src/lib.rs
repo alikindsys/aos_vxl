f1a7d408986cd161fb25f51dcdba5a76d74c7125
roridev <prates.aliceic@gmail.com>
2022-07-08 05:35:03 -0300

    Add `read_write_map` function

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ mod lib {
     use std::io::{Cursor, Error, Read};
     use std::iter::Map;
     use bytestream::ByteOrder::LittleEndian;
-    use bytestream::StreamReader;
+    use bytestream::{StreamReader, StreamWriter};
+    use sha2::{Sha512,Digest};
     use crate::data::data::VXL;
 
     #[test]
@@ -25,6 +26,32 @@ mod lib {
                 println!("Read [{}] successfully", name);
             }
         }
+        Ok(())
+    }
+
+    fn read_write_map(path: String) -> Result<(), Error> {
+        println!("Reading [{}]", path);
+        let file = read_file(path)?;
+        let mut buffer = Vec::new();
+
+        let mut hasher = Sha512::new();
+        let mut hasher2 = Sha512::new();
+
+        hasher.update(&*file.clone());
+
+        let map_data = read_map_data(file)?;
+
+        println!("Generating new VXL from internal data.");
+        map_data.write_to(&mut buffer, LittleEndian)?;
+        hasher2.update(&*buffer.clone());
+
+        let original_file_hash = hasher.finalize();
+        let output_file_hash = hasher2.finalize();
+
+        assert_eq!(original_file_hash[..], output_file_hash[..]);
+
+        println!("Generated files are equal to the original.");
+
         Ok(())
     }
 
